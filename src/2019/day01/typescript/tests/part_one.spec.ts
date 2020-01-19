187ccefefc2531a475a07130213d0e6df9f90059
AlexAegis <alexaegis@gmail.com>
2020-01-19 14:42:42 +0100

    updated packages

--- src/2019/day01/typescript/tests/part_one.spec.ts
+++ src/2019/day01/typescript/tests/part_one.spec.ts
@@ -2,13 +2,10 @@ import { read } from '@lib';
 import { day, results, year } from '..';
 import { runner } from '../part_one';
 
-describe(`${year} - Day ${day} - Part One`, () => {
-	test(
-        `should resolve to ${results.one.input} when using the input`,
-        async () => {
-            expect(await runner((await read(year, day)()).input)).toBe(results.one.input);
-        }
-    );
+describe(`2019 - Day 1 - Part One`, () => {
+	test(`should resolve to ${results.one.input} when using the input`, async () => {
+		expect(await runner((await read(year, day)()).input)).toBe(results.one.input);
+	});
 
 	test('should be that that the first example resolves to 2', async () => {
 		expect(await runner('12')).toBe(2);
